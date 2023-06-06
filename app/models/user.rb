b639edd177e2c67b57bd096ce5eb1b22218fafcf
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-05 19:44:27 -0700

    Write a validation test for first name, last name, email, password and email case match

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email, presence: true
-    validates :password, presence: true
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 6 }
 end
