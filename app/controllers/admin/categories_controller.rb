8c9f1fd9b1856d3e987b02ed511752d0a6c7009e
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-07 16:28:08 -0700

    Fix various functions after mentornship

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,7 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['AUTHENTICATION_USERNAME'], password: ENV['AUTHENTICATION_PASSWORD']
+
     def index
       @categories = Category.all
     end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    # abort @products.inspect
   end
 
   def show
